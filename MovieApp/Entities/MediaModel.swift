5a86bb7faf9a0c9e176b4b7bf42f02ab5b384d64
Erik Vera <apcxiii@gmail.com>
2019-10-22 15:49:05 -0500

    create Video Model

--- MovieApp/Entities/MediaModel.swift
+++ MovieApp/Entities/MediaModel.swift
@@ -21,9 +21,6 @@ class MediaResponse: Mappable {
     page <- map["page"]
     results <- map["results"]
   }
-  
-  
-  
 }
 
 class MediaModel: Mappable {
@@ -56,22 +53,23 @@ class MediaModel: Mappable {
   }
   
   func mapping(map: Map) {
-    id         <- map ["id"]
-    posterPath <- map["poster_path"]
-    adult      <- map["adult"]
-    overview <- map["overview"]
-    releaseDate <- map["release_date"]
-    originalTitle <- map["original_title"]
+    id              <- map ["id"]
+    posterPath      <- map["poster_path"]
+    adult           <- map["adult"]
+    overview        <- map["overview"]
+    releaseDate     <- map["release_date"]
+    originalTitle   <- map["original_title"]
     originalLanguage <- map["original_language"]
-    title <- map["title"]
-    backdropPath <- map["backdrop_path"]
-    popularity <- map["popularity"]
-    voteCount <- map["vote_count"]
-    voteAverage <- map["vote_average"]
+    title           <- map["title"]
+    backdropPath    <- map["backdrop_path"]
+    popularity      <- map["popularity"]
+    voteCount       <- map["vote_count"]
+    voteAverage     <- map["vote_average"]
     
-    name <- map["name"]
-    originalName <- map["original_name"]
-    firstAirDate <- map["first_air_date"]
+    //TV SHOWS
+    name            <- map["name"]
+    originalName    <- map["original_name"]
+    firstAirDate    <- map["first_air_date"]
   }
   
   
---
+++ MovieApp/Entities/VideoModel.swift
@@ -0,0 +1,48 @@
+//
+//  VideoModel.swift
+//  MovieApp
+//
+//  Created by Erik on 22/10/19.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+
+class VideoResponse: Mappable {
+  required init?(map: Map) {
+  }
+  
+  func mapping(map: Map) {
+    id      <- map["id"]
+    results <- map["result"]
+  }
+  
+  var id: Int?
+  var results: [VideoModel]?
+}
+
+class VideoModel: Mappable {
+  required init?(map: Map) {
+  }
+  
+  
+  
+  var id :String?
+  var iso6391: String?
+  var iso31661: String?
+  var key: String?
+  var name: String?
+  var site: String?
+  
+  func mapping(map: Map) {
+    id        <- map["id"]
+    iso6391   <- map["iso_639_1"]
+    iso31661  <- map["iso_3166_1"]
+    key       <- map["key"]
+    name      <- map["name"]
+    site      <- map["site"]
+  }
+  
+}
